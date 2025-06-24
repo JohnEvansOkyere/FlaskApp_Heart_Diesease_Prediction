0977e48c1c11149e0093987e9fbc6ec2e9883a77
JohnEvansOkyere <148287702+JohnEvansOkyere@users.noreply.github.com>
2025-06-24 13:22:35 +0000

    Update CORS to allow Vercel and localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,11 +13,15 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend URL
+    allow_origins=[
+        "http://localhost:3000", 
+        "https://flask-app-heart-diesease-prediction.vercel.app"  # your Vercel frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Initialize the predictor
 predictor = HeartDiseasePredictor()
